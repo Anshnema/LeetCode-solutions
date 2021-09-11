88d1709fc4bba3ff7cb7446559bf68c0e229f0a0
Anshuman <anshumannema01@gmail.com>
2021-09-11 20:45:23 +0530

    Creatd group_anagrams.py

---
+++ group_anagrams.py
@@ -0,0 +1,22 @@
+"""
+Ques. Group Anagrams 
+Link - https://leetcode.com/problems/group-anagrams/
+
+=> n = size of strs; k = max length of a string
+T.C. = O(nk) => Length of each string in n is counted 
+S.C. = O(nk) => Information for each n and k is stored in the dictionary
+"""
+def groupAnagrams(self, strs):
+    soln = dict()
+        
+    for string in strs:
+        count = [0] * 26
+        
+        for i in string:
+            count[ord(i) - ord("a")] += 1
+            
+        if(tuple(count) not in soln.keys()):
+            soln[tuple(count)] = [string]
+        else:
+            soln[tuple(count)].append(string)
+    return soln.values()
