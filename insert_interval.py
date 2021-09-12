97a5b2eb3f5c177debeb201106eb554705161aad
Anshuman <anshumannema01@gmail.com>
2021-09-12 15:51:03 +0530

    Created insert_interval.py

--- insert_interval.py
+++ insert_interval.py
@@ -1,3 +1,21 @@
 """
-
+Ques. Insert Interval
+T.C. = O(n)
+S.C. = O(n)
+Link - https://leetcode.com/problems/insert-interval/
 """
+def insert(self, intervals, newInterval):
+        ans = []
+        for i in range(len(intervals)):
+            if(intervals[i][1] < newInterval[0]):
+                ans.append(intervals[i])
+            elif(intervals[i][0] > newInterval[1]):
+                ans.append(newInterval)
+                return ans + intervals[i:] 
+            else:
+                if(intervals[i][1] >= newInterval[0] or intervals[i][0] <= newInterval[1]):
+                    newInterval[0] = min(intervals[i][0], newInterval[0])
+                    newInterval[1] = max(intervals[i][1], newInterval[1])
+        
+        ans.append(newInterval)
+        return ans 
