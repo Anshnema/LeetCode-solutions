a0fd0f891919ef2ee0084964c2456d891672bf8a
Anshuman <anshumannema01@gmail.com>
2021-09-17 21:34:30 +0530

    Created peak_index_in_a_mountain_array.py

---
+++ peak_index_in_a_mountain_array.py
@@ -0,0 +1,19 @@
+"""
+Ques 2. Peak index in mountain array 
+T.C = O(log n) 
+S.C = O(1)
+link - https://leetcode.com/problems/peak-index-in-a-mountain-array/
+"""
+def peakIndexInMountainArray(self, arr):
+        left = 0
+        right = len(arr) - 1
+        ans = - 1 
+        
+        while left <= right:
+            mid = (left + right) // 2 
+            if(arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]):
+                return mid 
+            elif(arr[mid] < arr[mid + 1]):
+                left = mid + 1 
+            else:
+                right = mid - 1
