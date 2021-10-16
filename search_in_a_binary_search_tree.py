675b704b86f930c4b04015c0237021dcae83cb0f
Anshuman <anshumannema01@gmail.com>
2021-10-16 15:54:11 +0530

    Created search_in_a_binary_search_tree.py

---
+++ search_in_a_binary_search_tree.py
@@ -0,0 +1,17 @@
+"""
+Ques. Search in a Binary search Tree 
+T.C. = O(log n) 
+S.C. = O(1)
+Link - https://leetcode.com/problems/search-in-a-binary-search-tree/
+"""
+class Solution:
+    def searchBST(self, root, val):
+        while root != None:
+            if(root.val == val):
+                return root 
+            elif(root.val > val):
+                root = root.left 
+            else:
+                root = root.right 
+        
+        return 
